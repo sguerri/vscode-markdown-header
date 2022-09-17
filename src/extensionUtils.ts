ddf7d93ef7e4d7867c9c4aa6f57c6830d44c360a
Sébastien GUERRI <13090870+sguerri@users.noreply.github.com>
2022-09-17 22:30:55 +0200

    Add license details

--- src/extensionUtils.ts
+++ src/extensionUtils.ts
@@ -1,3 +1,19 @@
+// Copyright (C) 2022 Sebastien Guerri
+//
+// This file is part of markdown-header.
+//
+// markdown-header is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// any later version.
+//
+// markdown-header is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with markdown-header. If not, see <https://www.gnu.org/licenses/>.
 
 export function generateId(length: number = 12): string
 {
